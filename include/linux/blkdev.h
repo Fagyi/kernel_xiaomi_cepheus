e921d34d45b601385b3020950b35190060eb5f51
Panchajanya1999 <panchajanya@azure-dev.live>
2021-07-14 16:02:49 +0200

    blkdev: Enable Write Back Caching Write back caching is a popular technique which copies data to higher level caches, backing store or memory.
    As per an article in Techopedia [1], enabling Write-Back Caching improves
    or reduces write operations to and from a cache and it's data source.
    
    [1] - https://www.techopedia.com/definition/1138/write-back-cache
    
    Change-Id: I0140736d3f921d5e0275f6154219009f84b6cb8b
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>
    Signed-off-by: negrroo <mohammedaelnaggar1@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -660,9 +660,11 @@ struct request_queue {
 #define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_NONROT)       |	\
 				 (1 << QUEUE_FLAG_STACKABLE)	|	\
 				 (1 << QUEUE_FLAG_SAME_COMP)    |	\
+				 (1 << QUEUE_FLAG_WC))
 
 #define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_STACKABLE)	|	\
-				 (1 << QUEUE_FLAG_SAME_COMP))
+				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
+				 (1 << QUEUE_FLAG_WC))
 
 /*
  * @q->queue_lock is set while a queue is being initialized. Since we know
